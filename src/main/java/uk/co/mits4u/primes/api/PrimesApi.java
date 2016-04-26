dd58a58194c91488650c336b0711f304879ff2d4
Edvinas <edvinas.veretinskas@gmail.com>
2016-04-26 17:57:32 +0100

    Setting up Spring-Boot application skeleton

---
+++ src/main/java/uk/co/mits4u/primes/api/PrimesApi.java
@@ -0,0 +1,6 @@
+package uk.co.mits4u.primes.api;
+
+public interface PrimesApi {
+
+
+}
---
+++ src/test/java/uk/co/mits4u/primes/api/PrimesApiIT.java
@@ -0,0 +1,10 @@
+package uk.co.mits4u.primes.api;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Edvinas on 26/04/2016.
+ */
+public class PrimesApiIT {
+
+}
