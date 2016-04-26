b1e0da89735a90b12492b739704b57c326f87308
Edvinas <edvinas.veretinskas@gmail.com>
2016-04-26 21:09:32 +0100

    Adding Swagger REST API

---
+++ src/main/java/uk/co/mits4u/primes/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package uk.co.mits4u.primes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig {
+
+    @Bean
+    public WebMvcConfigurerAdapter forwardToIndex() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("redirect:/primes-api/index.html");
+            }
+        };
+    }
+}
