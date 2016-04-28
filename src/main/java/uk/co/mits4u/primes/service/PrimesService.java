18863497baecf9a37713de0042ea878606a71747
Edvinas <edvinas.veretinskas@gmail.com>
2016-04-28 13:46:38 +0100

    Adding some basic logging capturing input, results and execution time

--- src/main/java/uk/co/mits4u/primes/service/PrimesService.java
+++ src/main/java/uk/co/mits4u/primes/service/PrimesService.java
@@ -2,6 +2,7 @@ package uk.co.mits4u.primes.service;
 
 import com.google.common.collect.ImmutableSortedSet;
 import org.apache.commons.lang3.Validate;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 import uk.co.mits4u.primes.api.AlgorithmName;
 import uk.co.mits4u.primes.api.PrimesApi;
@@ -10,9 +11,12 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.*;
 
+import static java.lang.String.format;
+
 @Service
 public class PrimesService implements PrimesApi {
 
+    private static Logger logger = Logger.getLogger(PrimesService.class);
     private Map<AlgorithmName, PrimeStrategy> strategies;
     @Resource
     private NumberValidator numberValidator;
@@ -31,21 +35,35 @@ public class PrimesService implements PrimesApi {
     @Override
     public boolean isPrime(int numberToCheck, AlgorithmName algorithmName) {
 
+        String params = format("number=[%s], algorithm=[%s]", numberToCheck, algorithmName);
+        logger.info("starting isPrime with [" + params + "]");
+        long start = System.currentTimeMillis();
+
         numberValidator.validateNumber(numberToCheck);
         PrimeStrategy primeStrategy = resolveStrategy(algorithmName);
+        boolean isPrime = primeStrategy.isPrime(numberToCheck);
 
-        return primeStrategy.isPrime(numberToCheck);
+        long timeTaken = System.currentTimeMillis() - start;
+        logger.info(format("finished isPrime with [%s] in %s ms -> result: [%s]", params, timeTaken, isPrime));
 
+        return isPrime;
     }
 
     @Override
     public Collection<Integer> getPrimesInRange(int floor, int ceiling, AlgorithmName algorithmName) {
+
+        String params = format("range=[%s, %s], algorithm=[%s]", floor, ceiling, algorithmName);
+        logger.info("starting calculation with [" + params + "]");
+        long start = System.currentTimeMillis();
 
         numberValidator.validateRange(floor, ceiling);
         PrimeStrategy primeStrategy = resolveStrategy(algorithmName);
 
         Collection<Integer> allPrimes = primeStrategy.generatePrimes(ceiling);
         ImmutableSortedSet<Integer> primesInRange = ImmutableSortedSet.copyOf(allPrimes).subSet(floor, true, ceiling, true);
+
+        long timeTaken = System.currentTimeMillis() - start;
+        logger.info(format("ended calculation with [%s] in %s ms -> found %s primes ", params, timeTaken, primesInRange.size()));
 
         return primesInRange;
 
