6db1e22dd66c3382fa7a5e8990c04634acd9c46d
vereedv <edvinas.veretinskas@gmail.com>
2017-08-14 19:47:17 +0100

    Reworking redirection so that it picks up scheme as well

--- src/main/java/uk/co/mits4u/primes/service/IndexController.java
+++ src/main/java/uk/co/mits4u/primes/service/IndexController.java
@@ -1,5 +1,6 @@
 package uk.co.mits4u.primes.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -9,7 +10,25 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class IndexController {
     @GetMapping("/")
-    public RedirectView redirect(HttpServletRequest request){
-        return new RedirectView(request.getRequestURL().toString()+"primes-api/index.html");
+    public RedirectView redirect(HttpServletRequest request) {
+
+        StringBuilder sb = new StringBuilder();
+
+        if (StringUtils.isNoneEmpty(request.getHeader("X-Forwarded-Host"))) {
+            sb.append(request.getHeader("X-Forwarded-Proto"));
+            sb.append("://");
+            sb.append(request.getHeader("X-Forwarded-Host"));
+            sb.append(":");
+            sb.append(request.getHeader("X-Forwarded-Port"));
+            sb.append("/");
+        } else {
+            sb.append(request.getRequestURL().toString());
+        }
+
+        sb.append("primes-api/index.html");
+
+
+
+        return new RedirectView(sb.toString());
     }
 }
